c4221edeb49469bb3c90b9c67f954e716955d927
shengs <shery.sheng@emc.com>
2017-02-13 13:54:56 +0800

    add wechat

---
+++ main.py
@@ -0,0 +1,44 @@
+# #coding=utf8
+# import requests
+# import itchat
+#
+# KEY = '8edce3ce905a4c1dbb965e6b35c3834d'
+#
+# def get_response(msg):
+#     apiUrl = 'http://www.tuling123.com/openapi/api'
+#     data = {
+#         'key'    : KEY,
+#         'info'   : msg,
+#         'userid' : 'wechat-robot',
+#     }
+#     try:
+#         r = requests.post(apiUrl, data=data).json()
+#         return r.get('text')
+#     except:
+#         return
+#
+# @itchat.msg_register(itchat.content.TEXT)
+# def tuling_reply(msg):
+#     defaultReply = 'I received: ' + msg['Text']
+#     reply = get_response(msg['Text'])
+#     return reply or defaultReply
+#
+# itchat.auto_login(hotReload=True)
+# itchat.run(
+
+#
+# import itchat
+#
+#
+# @itchat.msg_register(itchat.content.TEXT)
+# def print_content(msg):
+#     print(msg['Text'])
+#
+# itchat.auto_login()
+# itchat.run()
+
+import itchat
+
+itchat.auto_login(enableCmdQR=True)
+
+itchat.send('Hello, filehelper', toUserName='filehelper')
